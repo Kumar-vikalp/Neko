4cc651553edae67e97fe7f61fff0bff156059978
Moanster <moanster.telegram@gmail.com>
2026-03-15 09:34:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    output: 'standalone',
-    serverComponentsExternalPackages: ['shiki']
-  },
+  serverExternalPackages: ['shiki'],
+
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
